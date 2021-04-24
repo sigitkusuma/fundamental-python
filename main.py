97bdfebf18c2f5b123132d4655b5252bbb0aa1de
sigitkusuma <sigitsda71@gmail.com>
2021-04-24 20:19:37 +0700

    test

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+print("Hello World")
+
+try:
+    r = requests.get('https://goog le.com')
+    print(r.status_code)
+    if r.status_code == 200:
+        print(r.text)
+except Exception as s:
+    print("ada error", s)
